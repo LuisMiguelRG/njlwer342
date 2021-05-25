881611ec47e85ac26a75b7c3b4419e7ac5c1ec0b
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-05-25 18:55:15 -0500

    incrementa RG

--- src/App.js
+++ src/App.js
@@ -3,24 +3,36 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  state{
+    String: "",
+    task: ['Sacar la ropa', 'Hacer la cama','Leer un rato']
+  }
+
+  onsubmit(e) {
+    this.setState({
+      task: [...this.state.task, this.state.String],
+      String: ''
+    })
+    e.preventDefault()
+  }
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.task.map((e,i) => <li key={i}>{e}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.onSubmit.bind(this)}>
+             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" 
+               value={this.state.String}
+               onChange={this.handleChange.bind(this)}
+             />
            </form>
         </div>
       </div>
     )
   }
 }
-
 
 export default App;
